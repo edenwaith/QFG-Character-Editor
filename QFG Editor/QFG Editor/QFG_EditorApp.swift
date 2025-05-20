1963549c455b95e9aeae14a6da4b865eabe54ab4
Chad Armstrong <chad@edenwaith.com>
2025-05-19 22:14:09 -0600

    Initial project creation

---
+++ QFG Editor/QFG Editor/QFG_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QFG_EditorApp.swift
+//  QFG Editor
+//
+//  Created by Chad Armstrong on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct QFG_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QFG Editor/QFG EditorTests/QFG_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  QFG_EditorTests.swift
+//  QFG EditorTests
+//
+//  Created by Chad Armstrong on 5/19/25.
+//
+
+import Testing
+@testable import QFG_Editor
+
+struct QFG_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
